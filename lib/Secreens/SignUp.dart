d3bdc75b9365aa473dcc31858f9c1b4e9dc4181d
waelalq <wael.abuomr@gmail.com>
2022-12-29 01:33:05 +0200

    send POST request API

--- lib/Secreens/SignUp.dart
+++ lib/Secreens/SignUp.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_notes/Serveses/Auth.dart';
+import 'package:provider/provider.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -8,8 +10,6 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
-
-
   TextEditingController _nameController = new TextEditingController();
   TextEditingController _emailController = new TextEditingController();
   TextEditingController _passwordController = new TextEditingController();
@@ -27,31 +27,37 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
-shape: Border(bottom: BorderSide(color: Colors.greenAccent,width: 1)),
-        title: Center(child: Text("Sign up",style: TextStyle(color: Colors.black54),)),
+        shape: Border(bottom: BorderSide(color: Colors.greenAccent, width: 1)),
+        title: Center(
+            child: Text(
+          "Sign up",
+          style: TextStyle(color: Colors.black54),
+        )),
       ),
       body: SingleChildScrollView(
         child: Form(
           key: _formKey,
           child: Container(
-
-
             margin: EdgeInsets.all(10),
             child: Column(
               children: [
-
                 Container(
                   padding: EdgeInsets.all(15),
-                  decoration: BoxDecoration(color: Color(0xFFF1F8E9),borderRadius: BorderRadius.circular(25)),
-                  margin: EdgeInsets.only(top: 20,bottom: 30,left: 10,right: 10),
+                  decoration: BoxDecoration(
+                      color: Color(0xFFF1F8E9),
+                      borderRadius: BorderRadius.circular(25)),
+                  margin:
+                      EdgeInsets.only(top: 20, bottom: 30, left: 10, right: 10),
                   child: Column(
-                    
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Image(image: NetworkImage("https://www.pngmart.com/files/7/Note-PNG-Clipart.png"),height: 150,),
+                      Image(
+                        image: NetworkImage(
+                            "https://www.pngmart.com/files/7/Note-PNG-Clipart.png"),
+                        height: 150,
+                      ),
                       TextFormField(
                         validator: (value) {
                           if (value == null || value.isEmpty) {
@@ -63,7 +69,9 @@ shape: Border(bottom: BorderSide(color: Colors.greenAccent,width: 1)),
                         decoration: InputDecoration(
                           hintText: "Full name",
                           prefixIcon: Icon(Icons.person),
-                          border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
+                          border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15))),
                         ),
                       ),
                       Padding(padding: EdgeInsets.all(10)),
@@ -79,7 +87,9 @@ shape: Border(bottom: BorderSide(color: Colors.greenAccent,width: 1)),
                         decoration: InputDecoration(
                             hintText: "Email",
                             prefixIcon: Icon(Icons.email_outlined),
-                            border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(15)))),
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15)))),
                       ),
                       Padding(padding: EdgeInsets.all(10)),
                       TextFormField(
@@ -94,47 +104,57 @@ shape: Border(bottom: BorderSide(color: Colors.greenAccent,width: 1)),
                         decoration: InputDecoration(
                             hintText: "Password",
                             prefixIcon: Icon(Icons.password),
-                            border:
-                                OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(15)))),
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15)))),
                       ),
-SizedBox(width: 10,height: 20,)
-,
-                       DropdownButtonFormField(
-                          decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(15)))),
-                          icon: Icon(Icons.type_specimen_sharp),
-                          hint: Text("please select account "),
-                          isExpanded: true,
-                          items: accountType
-                              .map((e) => DropdownMenuItem(
-                                    child: Text("$e"),
-                                    value: e,
-                                  ))
-                              .toList(),
-                          onChanged: (val) {
-                            setState(() {
-                              selectedAcount = val;
-                            });
-                          },
-                          value: selectedAcount,
-                        ),
-Padding(padding: EdgeInsets.all(10)),
+                      SizedBox(
+                        width: 10,
+                        height: 20,
+                      ),
+                      DropdownButtonFormField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15)))),
+                        icon: Icon(Icons.type_specimen_sharp),
+                        hint: Text("please select account "),
+                        isExpanded: true,
+                        items: accountType
+                            .map((e) => DropdownMenuItem(
+                                  child: Text("$e"),
+                                  value: e,
+                                ))
+                            .toList(),
+                        onChanged: (val) {
+                          setState(() {
+                            selectedAcount = val;
+                          });
+                        },
+                        value: selectedAcount,
+                      ),
+                      Padding(padding: EdgeInsets.all(10)),
                       ElevatedButton(
-
                           onPressed: () {
                             Map user = {
                               'name': _nameController.text,
-                              'email':_emailController.text,
-                              'password':_passwordController.text,
-                              'accountType':selectedAcount
-
+                              'email': _emailController.text,
+                              'password': _passwordController.text,
+                              'accountType': selectedAcount
                             };
                             if (_formKey.currentState!.validate()) {
                               print(user);
-                            }
 
+                             Provider.of<Auth>(context,listen: false).SignUp(user);
+                            }
                           },
-                          style: ElevatedButton.styleFrom(backgroundColor: Color(0xFFC8E6C9),minimumSize: Size(380,50)),
-                          child: Text("Submit",style: TextStyle(color: Colors.black54),))
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xFFC8E6C9),
+                              minimumSize: Size(380, 50)),
+                          child: Text(
+                            "Submit",
+                            style: TextStyle(color: Colors.black54),
+                          ))
                     ],
                   ),
                 ),
--- lib/Serveses/Auth.dart
+++ lib/Serveses/Auth.dart
@@ -1,15 +1,22 @@
+
 import 'package:flutter/cupertino.dart';
+import 'package:my_notes/Secreens/DioRequest.dart';
 
 class Auth extends ChangeNotifier{
   bool _isLogin=false;
   bool get authantucated=>_isLogin;
   void login(Map Card){
+
     _isLogin=true;
     notifyListeners();
   }
   void logout(){
     _isLogin=false;
     notifyListeners();
+  }
+
+  void SignUp( Map creds) async {
+   await DioRequest.Request().post('/signup',data: creds);
   }
 
 }
